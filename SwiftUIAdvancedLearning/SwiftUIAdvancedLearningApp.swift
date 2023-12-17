7ec326d351aefd824e24eb45b5638b0e9f10b735
Berkay Tuncel <berkaytncl98@gmail.com>
2023-12-17 14:20:42 +0300

    Initial Commit

---
+++ SwiftUIAdvancedLearning/SwiftUIAdvancedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAdvancedLearningApp.swift
+//  SwiftUIAdvancedLearning
+//
+//  Created by Berkay Tuncel on 17.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAdvancedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

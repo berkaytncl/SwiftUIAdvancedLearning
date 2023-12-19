989152e10d3fa1a4e2d9dca24f0bc2ab6fe9bb1f
Berkay Tuncel <berkaytncl98@gmail.com>
2023-12-20 01:03:13 +0300

    Add notes and examples for the @ViewBuilder

---
+++ SwiftUIAdvancedLearning/ViewBuilderBootcamp.swift
@@ -0,0 +1,153 @@
+//
+//  ViewBuilderBootcamp.swift
+//  SwiftUIAdvancedLearning
+//
+//  Created by Berkay Tuncel on 19.12.2023.
+//
+
+import SwiftUI
+
+struct HeaderViewRegular: View {
+    
+    let title: String
+    let description: String?
+    let iconName: String?
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(title)
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+            
+            if let description {
+                Text(description)
+                    .font(.callout)
+            }
+            
+            if let iconName {
+                Image(systemName: iconName)
+            }
+            
+            RoundedRectangle(cornerRadius: 5)
+                .frame(height: 2)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding()
+    }
+}
+
+struct HeaderViewGeneric<Content: View>: View {
+    
+    let title: String
+    let content: Content
+    
+    init(title: String, @ViewBuilder content: () -> Content) {
+        self.title = title
+        self.content = content()
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(title)
+                .font(.largeTitle)
+                .fontWeight(.semibold)
+            
+            content
+            
+            RoundedRectangle(cornerRadius: 5)
+                .frame(height: 2)
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .padding()
+    }
+}
+
+struct CustomHStack<Content: View>: View {
+    
+    let content: Content
+    
+    init(@ViewBuilder content: () -> Content) {
+        self.content = content()
+    }
+    
+    var body: some View {
+        HStack {
+            content
+        }
+    }
+}
+
+struct ViewBuilderBootcamp: View {
+    var body: some View {
+        VStack {
+            HeaderViewRegular(title: "Title", description: "Description", iconName: "heart.fill")
+            HeaderViewRegular(title: "Title2", description: nil, iconName: nil)
+            
+            HeaderViewGeneric(title: "Generic Title") {
+                HStack {
+                    Text("Hi")
+                    Image(systemName: "heart.fill")
+                    Text("Hi")
+                }
+            }
+            
+            CustomHStack {
+                Text("Hi")
+                Text("Hi")
+            }
+            
+            HStack {
+                Text("Hi")
+                Text("Hi")
+            }
+            
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+//    ViewBuilderBootcamp()
+    LocalViewBuilder(type: .three)
+}
+
+struct LocalViewBuilder: View {
+    
+    enum ViewType {
+        case one, two, three
+    }
+    
+    let type: ViewType
+    
+    var body: some View {
+        VStack {
+            headerSection
+        }
+    }
+    
+    @ViewBuilder private var headerSection: some View {
+        switch type {
+        case .one:
+            viewOne
+        case .two:
+            viewTwo
+        case .three:
+            viewThree
+        }
+    }
+    
+    private var viewOne: some View {
+        Text("One!")
+    }
+    
+    private var viewTwo: some View {
+        VStack {
+            Text("Twoo")
+            Image(systemName: "heart.fill")
+        }
+    }
+    
+    private var viewThree: some View {
+        Image(systemName: "heart.fill")
+    }
+}
